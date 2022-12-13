7884b261c8a30c4fce63b5e7d12f564c4e0a4b90
lowbrain <83059975+lowbrain@users.noreply.github.com>
2022-12-13 21:56:10 +0900

    spring init

---
+++ src/main/java/com/example/saml2/Saml2Application.java
@@ -0,0 +1,13 @@
+package com.example.saml2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saml2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Saml2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saml2/Saml2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saml2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saml2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
