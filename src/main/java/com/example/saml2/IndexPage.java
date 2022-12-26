5cbb4df223a0432de375dd9cf5b3212ac8d6b5c8
LowBrain <83059975+lowbrain@users.noreply.github.com>
2022-12-26 22:36:59 +0900

    初期ページを定義

--- src/main/java/com/example/saml2/IndexPage.java
+++ src/main/java/com/example/saml2/IndexPage.java
@@ -2,21 +2,23 @@ package com.example.saml2;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexPage {
 
   @RequestMapping("/")
-  public AuthData index(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal) {
+  public String index(Model model, @AuthenticationPrincipal Saml2AuthenticatedPrincipal principal) {
     AuthData data = AuthData.builder()
         .name(principal.getAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"))
         .emailaddress(principal.getAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"))
         .givenname(principal.getAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"))
         .surname(principal.getAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname"))
         .build();
-    return data;
+    model.addAttribute("authInfo", data);
+    return "index";
   }
   
 }
