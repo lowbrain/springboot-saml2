849ca5bacbc06cd9084221149c0b0f6468c2599e
LowBrain <83059975+lowbrain@users.noreply.github.com>
2022-12-29 01:37:37 +0900

    戻りURLを明示的に指定

--- src/main/java/com/example/saml2/IndexPage.java
+++ src/main/java/com/example/saml2/IndexPage.java
@@ -1,5 +1,6 @@
 package com.example.saml2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexPage {
+
+  @Value("${saml2.return.url}")
+	private String retUrl;
 
   @RequestMapping("/")
   public String index(Model model, @AuthenticationPrincipal Saml2AuthenticatedPrincipal principal) {
@@ -19,6 +23,7 @@ public class IndexPage {
         .build();
     model.addAttribute("name", data.getName());
     model.addAttribute("authInfo", data);
+    model.addAttribute("retUrl", retUrl);
     return "index";
   }
 }
